c4362214747601e5740af4b514224023c9030a6c
Jeremy Vinai <jvinai@xebia.fr>
2014-12-17 11:54:33 +0100

    change gulpfile quote

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp = require("gulp"),
-  jshint = require("gulp-jshint"),
+var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
   mocha = require('gulp-mocha');
 
-gulp.task("jshint", function () {
-  gulp.src("lib/*.*")
+gulp.task('jshint', function () {
+  gulp.src('lib/*.*')
     .pipe(jshint())
-    .pipe(jshint.reporter("default"))
-    .pipe(jshint.reporter("fail"));
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
-gulp.task("test", function() {
+gulp.task('test', function() {
   return gulp.src('test/*.*', {read: false})
     .pipe(mocha({reporter: 'nyan'}));
 })
-gulp.task("default", ["jshint", "test"]);
+gulp.task('default', ['jshint', 'test']);
