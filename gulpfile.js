787b092091d1b247e86df443f3c656c8bb7bd356
Jeremy Vinai <jvinai@xebia.fr>
2014-12-17 11:22:39 +0100

    Add gulp, jshint and gulp-mocha

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp"),
+  jshint = require("gulp-jshint"),
+  mocha = require('gulp-mocha');
+
+gulp.task("jshint", function () {
+  gulp.src("lib/*.*")
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"))
+    .pipe(jshint.reporter("fail"));
+});
+
+gulp.task("test", function() {
+  return gulp.src('test/*.*', {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+})
+gulp.task("default", ["jshint"]);
